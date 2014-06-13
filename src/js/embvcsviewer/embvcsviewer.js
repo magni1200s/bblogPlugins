4845efc6747780fea0b3e2dc689676bafd2bc78e
magni1200s <magni1200s@outlook.com>
2014-06-13 18:37:30 +0900

    Adding support for Battlefield Hardline
    Adding a new mode 'bfh'
    Change insertion point

--- src/js/embvcsviewer/embvcsviewer.js
+++ src/js/embvcsviewer/embvcsviewer.js
@@ -4,7 +4,7 @@ BBLog.handle("add.plugin", {
 	/** @type 	{String}		The extension's name.  		*/
 	name: 'Emblemers VC Server Viewer',
 	/** @type 	{String} 		The version string.		*/
-	version: '1.0.0.1',
+	version: '1.0.0.2',
 	/** @type 	{Object} 		BBL Translation stuff.		*/
 	translations: {},
 	/** @type 	{Object} 		Config flags from BBL.		*/
@@ -20,13 +20,13 @@ BBLog.handle("add.plugin", {
 	
 	init : function(instance) {
 		this.instance = instance
-		if (BBLog.cache('mode') === 'bf4') {
+		if ((BBLog.cache('mode') === 'bf4') || (BBLog.cache('mode') === 'bfh')) {
 			this.setContainer()
 		}
 	},
 	
 	domchange: function () {
-		if (BBLog.cache('mode') === 'bf4')
+		if ((BBLog.cache('mode') === 'bf4') || (BBLog.cache('mode') === 'bfh'))
 			this.setContainer()
 	},
 	
@@ -38,7 +38,7 @@ BBLog.handle("add.plugin", {
 				var code = '<div id="main-loggedin-embvcsiewer" class="main-loggedin-embvcsiewer">'
 					code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=173.199.82.240:9308&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=0&currentPlayersHeight=160&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="348"></iframe>'
 					code += '</div>'
-				$(".advirticement.battlelog-bf4-homepage").before(code)
+				$(".span4.main-side-column").prepend(code)
 			}
 		}
 	}
