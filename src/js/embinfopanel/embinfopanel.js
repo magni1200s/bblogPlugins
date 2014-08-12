0e6c29eb9aa6467c1409e2f37303387f054e4ad5
magni1200s <magni1200s@outlook.com>
2014-08-13 02:43:38 +0900

    Adding a new game server
    Adding a new game server (i3D 64 slots)

--- src/js/embinfopanel/embinfopanel.js
+++ src/js/embinfopanel/embinfopanel.js
@@ -4,7 +4,7 @@ BBLog.handle("add.plugin", {
 	/** @type 	{String}		The extension's name.  		*/
 	name: 'Emblemers Platoon Information Panel',
 	/** @type 	{String} 		The version string.		*/
-	version: '1.0.0.0',
+	version: '1.0.1.0',
 	/** @type 	{Object} 		BBL Translation stuff.		*/
 	translations: {
 		"en": {
@@ -65,6 +65,7 @@ BBLog.handle("add.plugin", {
 					if (exitGsvViewer == 0) {
 						code += '<div id="main-loggedin-embgamesvrviewer" class="main-loggedin-embgamesvrviewer">'
 						code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=109.200.221.224:25640&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=1&currentPlayersHeight=160&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="472"></iframe>'
+						code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=109.200.221.195:25500&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
 						code += '</div>'
 					}
 				}
