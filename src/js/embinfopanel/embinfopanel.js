ba44cbe9a2d1ae151767d865086f0f0ff45fa3af
magni1200s <magni1200s@outlook.com>
2014-11-21 14:00:24 +0900

    URL change for temporary solution
    Due to Google Analytics issue, changed target XML hosting site to
    temporary one.

--- src/js/embinfopanel/embinfopanel.js
+++ src/js/embinfopanel/embinfopanel.js
@@ -4,7 +4,7 @@ BBLog.handle("add.plugin", {
 	/** @type 	{String}		The extension's name.  		*/
 	name: 'Emblemers Platoon Information Panel',
 	/** @type 	{String} 		The version string.		*/
-	version: '1.0.1.2',
+	version: '1.0.1.3',
 	/** @type 	{Object} 		BBL Translation stuff.		*/
 	translations: {
 		"en": {
@@ -55,7 +55,8 @@ BBLog.handle("add.plugin", {
 					var exitGalViewer = $("html").find(".main-loggedin-embgalleryviewer").length
 					if (exitGalViewer == 0) {
 						code += '<div id="main-loggedin-embgalleryviewer" class="main-loggedin-embgalleryviewer">'
-						code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2fprac%2dgadget%2egooglecode%2ecom%2ffiles%2fpinterest%2dslideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23ffffff&up_LCOL=%23ffffff&up_SCOL=%23444444&up_BCOL=%23444444&up_CCOL=%23444444"></iframe>'
+//						code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2fprac%2dgadget%2egooglecode%2ecom%2ffiles%2fpinterest%2dslideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23ffffff&up_LCOL=%23ffffff&up_SCOL=%23444444&up_BCOL=%23444444&up_CCOL=%23444444"></iframe>'
+						code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2framble%2esakura%2ene%2ejp%2femb%2fpinterest-slideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23ffffff&up_LCOL=%23ffffff&up_SCOL=%23444444&up_BCOL=%23444444&up_CCOL=%23444444"></iframe>'
 						code += '</div>'
 					}
 				}
