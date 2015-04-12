5c531c1e2c47ca100d788f5c7159b03a4dd4b210
magni1200s <magni1200s@outlook.com>
2015-04-12 17:01:55 +0900

    Added a new game server
    Added a new game server. Also changed some variable names.

--- src/js/embinfopanel/embinfopanel.js
+++ src/js/embinfopanel/embinfopanel.js
@@ -4,7 +4,7 @@ BBLog.handle("add.plugin", {
 	/** @type 	{String}		The extension's name.  		*/
 	name: 'Emblemers Platoon Information Panel',
 	/** @type 	{String} 		The version string.		*/
-	version: '1.0.2.0',
+	version: '1.0.2.1',
 	/** @type 	{Object} 		BBL Translation stuff.		*/
 	translations: {
 		"en": {
@@ -61,8 +61,8 @@ BBLog.handle("add.plugin", {
 				var code = ''
 
 				if (this.instance.storage('show.galleries')) {
-					var exitGalViewer = $("html").find(".main-loggedin-embgalleryviewer").length
-					if (exitGalViewer == 0) {
+					var isGalViewer = $("html").find(".main-loggedin-embgalleryviewer").length
+					if (isGalViewer == 0) {
 						code += '<div id="main-loggedin-embgalleryviewer" class="main-loggedin-embgalleryviewer">'
 						if (BBLog.cache('mode') === 'bf4') {
 //							code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2fprac%2dgadget%2egooglecode%2ecom%2ffiles%2fpinterest%2dslideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23ffffff&up_LCOL=%23ffffff&up_SCOL=%23444444&up_BCOL=%23444444&up_CCOL=%23444444"></iframe>'
@@ -75,14 +75,14 @@ BBLog.handle("add.plugin", {
 				}
 
 				if (this.instance.storage('show.gameservers')) {
-					var exitGsvViewer = $("html").find(".main-loggedin-embgamesvrviewer").length
-					if (exitGsvViewer == 0) {
+					var isGsvViewer = $("html").find(".main-loggedin-embgamesvrviewer").length
+					if (isGsvViewer == 0) {
 						code += '<div id="main-loggedin-embgamesvrviewer" class="main-loggedin-embgamesvrviewer">'
-//						if (BBLog.cache('mode') === 'bf4') {
+						if (BBLog.cache('mode') === 'bf4') {
 //							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=109.200.221.195:25500&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
-//						} else if (BBLog.cache('mode') === 'bfh') {
-//							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=109.200.221.195:25500&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
-//						}
+						} else if (BBLog.cache('mode') === 'bfh') {
+							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=161.202.101.86:25300&bgColor=FFFFFF&fontColor=333333&titleBgColor=FFFFFF&titleColor=000000&borderColor=BBBBBB&linkColor=091858&borderLinkColor=5C5C5C&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
+						}
 						code += '</div>'
 					}
 				}
