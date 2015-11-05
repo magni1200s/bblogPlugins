93618264273b086763d6b44fdb84b0676482aef3
magni1200s <magni1200s@outlook.com>
2015-11-05 13:13:49 +0900

    Voice Server Info updated
    Updated the latest voice server info

--- src/js/embinfopanel/embinfopanel.js
+++ src/js/embinfopanel/embinfopanel.js
@@ -4,7 +4,7 @@ BBLog.handle("add.plugin", {
 	/** @type 	{String}		The extension's name.  		*/
 	name: 'Emblemers Platoon Information Panel',
 	/** @type 	{String} 		The version string.		*/
-	version: '1.0.2.1',
+	version: '1.0.2.2',
 	/** @type 	{Object} 		BBL Translation stuff.		*/
 	translations: {
 		"en": {
@@ -81,7 +81,7 @@ BBLog.handle("add.plugin", {
 						if (BBLog.cache('mode') === 'bf4') {
 //							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=109.200.221.195:25500&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
 						} else if (BBLog.cache('mode') === 'bfh') {
-							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=161.202.101.86:25300&bgColor=FFFFFF&fontColor=333333&titleBgColor=FFFFFF&titleColor=000000&borderColor=BBBBBB&linkColor=091858&borderLinkColor=5C5C5C&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
+//							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=161.202.101.86:25300&bgColor=FFFFFF&fontColor=333333&titleBgColor=FFFFFF&titleColor=000000&borderColor=BBBBBB&linkColor=091858&borderLinkColor=5C5C5C&showMap=1&currentPlayersHeight=100&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="412"></iframe>'
 						}
 						code += '</div>'
 					}
@@ -92,9 +92,9 @@ BBLog.handle("add.plugin", {
 					if (exitVcsViewer == 0) {
 						code += '<div id="main-loggedin-embvcsiewer" class="main-loggedin-embvcsiewer">'
 						if (BBLog.cache('mode') === 'bf4') {
-							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=173.199.82.240:9308&bgColor=333333&fontColor=CCCCCC&titleBgColor=222222&titleColor=FF9900&borderColor=555555&linkColor=FFCC00&borderLinkColor=222222&showMap=0&currentPlayersHeight=160&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="348"></iframe>'
+							code += '<iframe src="https://discordapp.com/widget?id=107831378061570048&theme=dark" width="320" height="400" allowtransparency="true" frameborder="0"></iframe>'
 						} else if (BBLog.cache('mode') === 'bfh') {
-							code += '<iframe src="http://cache.www.gametracker.com/components/html0/?host=173.199.82.240:9308&bgColor=FFFFFF&fontColor=333333&titleBgColor=FFFFFF&titleColor=000000&borderColor=BBBBBB&linkColor=091858&borderLinkColor=5C5C5C&showMap=0&currentPlayersHeight=160&showCurrPlayers=1&showTopPlayers=0&showBlogs=0&width=320" frameborder="0" scrolling="no" width="320" height="348"></iframe>'
+							code += '<iframe src="https://discordapp.com/widget?id=107831378061570048&theme=light" width="320" height="400" allowtransparency="true" frameborder="0"></iframe>'
 						}
 						code += '</div>'
 					}
