a4109f3301aa8078a7e29f9043a728d934124c24
magni1200s <magni1200s@outlook.com>
2017-02-05 14:05:54 +0900

    Slideshow update
    Updated slideshow widget URL since the original widget hasn't work
    anymore

--- src/js/embinfopanel/embinfopanel.js
+++ src/js/embinfopanel/embinfopanel.js
@@ -4,7 +4,7 @@ BBLog.handle("add.plugin", {
 	/** @type 	{String}		The extension's name.  		*/
 	name: 'Emblemers Platoon Information Panel',
 	/** @type 	{String} 		The version string.		*/
-	version: '1.0.2.2',
+	version: '1.0.2.3',
 	/** @type 	{Object} 		BBL Translation stuff.		*/
 	translations: {
 		"en": {
@@ -65,10 +65,9 @@ BBLog.handle("add.plugin", {
 					if (isGalViewer == 0) {
 						code += '<div id="main-loggedin-embgalleryviewer" class="main-loggedin-embgalleryviewer">'
 						if (BBLog.cache('mode') === 'bf4') {
-//							code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2fprac%2dgadget%2egooglecode%2ecom%2ffiles%2fpinterest%2dslideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23ffffff&up_LCOL=%23ffffff&up_SCOL=%23444444&up_BCOL=%23444444&up_CCOL=%23444444"></iframe>'
-							code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2framble%2esakura%2ene%2ejp%2femb%2fpinterest-slideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23ffffff&up_LCOL=%23ffffff&up_SCOL=%23444444&up_BCOL=%23444444&up_CCOL=%23444444"></iframe>'
+							code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://viewer.headsup.news/?channelId=5896ab8f49d926405bca7d47"></iframe>'
 						} else if (BBLog.cache('mode') === 'bfh') {
-							code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://www.gmodules.com/gadgets/ifr?url=http%3a%2f%2framble%2esakura%2ene%2ejp%2femb%2fpinterest-slideshow%2exml&up_USER=magni1200s&up_BOARD=bf4-emblems-by-the-emblemers&up_PWH=300&up_PHT=300&up_LNK=http%3a%2f%2fwww%2epinterest%2ecom%2fmagni1200s%2fbf4%2demblems%2dby%2dthe%2demblemers%2f&up_TIT=Emblemers%20Gallery&up_DTime=&up_TTime=&up_RND=&up_CLP=Yes&up_NAB=Yes&up_TCOL=%23000000&up_LCOL=%23000000&up_SCOL=%23ffffff&up_BCOL=%23ffffff&up_CCOL=%23ffffff"></iframe>'
+							code += '<iframe style="width: 320px; height: 370px;" allowTransparency="true" frameborder="0" scrolling="no" src="http://viewer.headsup.news/?channelId=5896ab8f49d926405bca7d47"></iframe>'
 						}
 						code += '</div>'
 					}
